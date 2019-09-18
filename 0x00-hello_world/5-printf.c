6d1123696184092fac991971e5cb9b99cdbb14fa
danny099 <dalmariv01@gmail.com>
2019-09-18 18:32:41 +0000

    2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,10 @@
 *
 * Return: Always 0 (Success)
 */
+#include <stdio.h>
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
