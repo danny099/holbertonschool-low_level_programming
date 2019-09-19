1cb7c22a1eadc3a6b719c0cfb3c294a81d5a9253
danny099 <dalmariv01@gmail.com>
2019-09-19 20:46:40 +0000

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num <= 9)
+	{
+		putchar(num % 10 + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
