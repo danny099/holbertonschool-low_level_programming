d62eec99215c8ba2adcd8be47b3da901f9d5685b
danny099 <dalmariv01@gmail.com>
2019-09-23 22:44:07 +0000

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* _islower - what letter is lower
+*
+*
+* Return: On succes 0.
+* On error.
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
