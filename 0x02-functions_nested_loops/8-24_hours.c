3df489920950aea9eab99d7f80a3d07cbc85625c
danny099 <dalmariv01@gmail.com>
2019-09-24 00:27:21 +0000

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* print_alphabet x10 - writes the lowercase letters
+*
+*
+* Return: On success lowercase letters.
+* On error, dont return lowercase letters.
+*/
+void jack_bauer(void)
+{
+
+	int i = 0, j = 0;
+	
+	while(i < 24)
+	{
+		j = -1;
+		while(j < 59)
+		{
+			++j;
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+		i++;
+	}
+	
+}
