e5b86bdd95fb6f786bd38eeba98859f0e3878856
danny099 <dalmariv01@gmail.com>
2019-09-24 01:34:12 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* print_alphabet x10 - writes the lowercase letters
+*
+*
+* Return: On success lowercase letters.
+* On error, dont return lowercase letters.
+*/
+int add(int a, int b)
+{
+	int suma;
+
+	suma = a + b;
+	return (suma);
+}
