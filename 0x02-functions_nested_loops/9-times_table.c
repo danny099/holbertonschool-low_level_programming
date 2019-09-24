bb1d925f3801e23f50b4010cd4ae3f3d44859866
danny099 <dalmariv01@gmail.com>
2019-09-24 15:30:34 +0000

    9v2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,51 +8,44 @@
 */
 void times_table(void)
 {
-
 	int i, j, mult;
-	
-	i = 0;
 
-	while(i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
-		j = 0;
-		while(j <= 9)
+		for (j = 0; j <= 9; j++)
 		{
-			mult = i*j;
-			if(j == 0 && mult/10 == 0)
+			mult = i * j;
+			if (j == 0 && mult / 10 == 0)
 			{
-				_putchar(mult%10 + '0');
+				_putchar(mult % 10 + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
-			else if(j == 9 && mult/10 == 0)
+			else if (j == 9 && mult / 10 == 0)
 			{
 				_putchar(' ');
-				_putchar(mult%10 + '0');
+				_putchar(mult % 10 + '0');
 			}
-			else if (j == 9 && mult/10 != 0)
+			else if (j == 9 && mult / 10 != 0)
 			{
-				_putchar(mult/10 + '0');
-				_putchar(mult%10 + '0');
+				_putchar(mult / 10 + '0');
+				_putchar(mult % 10 + '0');
 			}
-			else if(mult/10 == 0)
+			else if (mult / 10 == 0)
 			{
 				_putchar(' ');
-				_putchar(mult%10 + '0');
+				_putchar(mult % 10 + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar(mult/10 + '0');
-				_putchar(mult%10 + '0');
+				_putchar(mult / 10 + '0');
+				_putchar(mult % 10 + '0');
 				_putchar(',');
 				_putchar(' ');
-
 			}
-			j++;
 		}
 		_putchar('\n');
-		i++;
 	}
 }
