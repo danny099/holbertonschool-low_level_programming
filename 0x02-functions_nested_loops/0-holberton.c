e260b871d1fcc8bbfa9bc868c6527c8c9acd226d
danny099 <dalmariv01@gmail.com>
2019-09-24 13:21:55 +0000

    0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -11,13 +11,15 @@
 
 int main(void)
 {
-	int i;
-	char hol[] = "Holberton";
-
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(hol[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
