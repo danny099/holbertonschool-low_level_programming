672a726ef567ba1b1a63f8309944e94129edbb23
danny099 <dalmariv01@gmail.com>
2019-09-24 15:09:30 +0000

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,29 +1,58 @@
 #include "holberton.h"
 /**
-* print_alphabet x10 - writes the lowercase letters
+* times_table - print 9 times table.
 *
 *
-* Return: On success lowercase letters.
-* On error, dont return lowercase letters.
+* Return: On succes print 9 times table.
+* On error, dont return times table.
 */
 void times_table(void)
 {
 
-	int i = 0, j = 0, mult = 0;
+	int i, j, mult;
 	
+	i = 0;
+
 	while(i <= 9)
 	{
+		j = 0;
 		while(j <= 9)
 		{
 			mult = i*j;
-			if(mult/10 == 0)
+			if(j == 0 && mult/10 == 0)
 			{
+				_putchar(mult%10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if(j == 9 && mult/10 == 0)
+			{
+				_putchar(' ');
+				_putchar(mult%10 + '0');
+			}
+			else if (j == 9 && mult/10 != 0)
+			{
+				_putchar(mult/10 + '0');
+				_putchar(mult%10 + '0');
+			}
+			else if(mult/10 == 0)
+			{
+				_putchar(' ');
+				_putchar(mult%10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(mult/10 + '0');
+				_putchar(mult%10 + '0');
+				_putchar(',');
+				_putchar(' ');
 
 			}
 			j++;
 		}
-		i++
+		_putchar('\n');
+		i++;
 	}
-	
-	
 }
