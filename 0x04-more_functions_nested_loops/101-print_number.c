117799ad80d870df61b0e484d190068f804f4d15
danny099 <dalmariv01@gmail.com>
2019-09-27 02:46:11 +0000

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+*print_number - print numbers
+*
+*@n:is a number
+* Return: On (succes) numbers 0-9.
+* On error, dont return numbers.
+*/
+void print_number(int n)
+{
+
+}
