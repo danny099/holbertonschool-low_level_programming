afdc0ffb9e3e1f18e831fc9f3df44fa09ff0dc8f
danny099 <dalmariv01@gmail.com>
2019-09-30 20:08:49 +0000

    12

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* swap_int - swap two ints with a pointers
+*
+*@a: is a pointer
+*@b: is a pointer
+* Return: On succes none.
+* On error.
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _strlen - return de lenght of a string
+*
+*@s: is a pointer
+* Return: On succes lenght oa a string.
+* On error.
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
