46213637af692dda5ff74f81df04f41186e850dd
danny099 <dalmariv01@gmail.com>
2019-09-30 22:18:10 +0000

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* print_rev - print reverse of a string
+*
+*@s: is a pointer
+* Return: On succes reverse of a string.
+* On error.
+*/
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+
+	for (j = i; j > 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
