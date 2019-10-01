5005cf74a1339c54a28555e6612f1d255f1d7ad0
danny099 <dalmariv01@gmail.com>
2019-10-01 15:50:07 +0000

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,20 +9,13 @@
 
 void puts_half(char *str)
 {
-	int i, j, div;
+	int i, j;
 
 	for (i = 0; str[i] != 0; i++)
 	{
 	}
-	if (i % 2 == 0)
-	{
-		div = i / 2;
-	}
-		else
-		{
-			div = (i - 1) / 2;
-		}
-	for (j = div; str[j] != 0; j++)
+	i++;
+	for (j = i / 2; str[j] != 0; j++)
 	{
 		_putchar(str[j]);
 	}
