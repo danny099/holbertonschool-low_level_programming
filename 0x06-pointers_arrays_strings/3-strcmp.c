9227d79379275dc9bbf39ae09a9ee3b9d41250bf
danny099 <dalmariv01@gmail.com>
2019-10-03 19:15:13 +0000

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,12 +14,12 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = -1;
-	do
-	{
+	do {
 		i++;
 		if (s1[i] == '\0')
+		{
 			return (s1[i] - s2[i]);
-	}
-	while (s1[i] == s2[i]);
+		}
+	} while (s1[i] == s2[i]);
 	return (s1[i] - s2[i]);
 }
