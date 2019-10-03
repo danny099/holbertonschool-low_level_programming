a16d537ceb5d5c5a4ecc46c91e0b87cdecb328e0
danny099 <dalmariv01@gmail.com>
2019-10-03 20:47:44 +0000

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+**reverse_array - reverse array
+*
+*@a: is a pointer
+*@n: is a number
+* Return: On succes array.
+* On error.
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+	n--;
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = a[i];
+		*(a + i) = a[n];
+		a[n] = tmp;
+		n--;
+	}
+}
