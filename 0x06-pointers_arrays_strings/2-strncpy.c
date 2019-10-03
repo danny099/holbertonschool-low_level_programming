5cbbd8ecd371cd6b9934ee425e0e2a97dfbe6d90
danny099 <dalmariv01@gmail.com>
2019-10-03 17:28:59 +0000

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  **_strncpy - copy a strings
+  *
+  *@dest: is a pointer
+  *@src: is a pointer
+  *@n: is a number
+  * Return: On succes string.
+  * On error.
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != 0 && i < n; i++)
+	{
+		*(dest + i) = src[i];
+	}
+	if (src[i] == '\0')
+	{
+		*(dest + i) = src[i];
+	}
+	return (dest);
+
+}
