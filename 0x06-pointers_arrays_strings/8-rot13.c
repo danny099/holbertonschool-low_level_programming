641cbeffbcf8f0ca45ab991b8cfabba8ed4808c5
danny099 <dalmariv01@gmail.com>
2019-10-04 04:12:40 +0000

    8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ **rot13 - string to upper
+ * @p: is a pointer
+ * Return: p
+ */
+char *rot13(char *p)
+{
+	int i, j;
+	char c1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char c2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		for (j = 0; c1[j] != '\0'; j++)
+		{
+			if (p[i] == c1[j])
+			{
+				p[i] = c2[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
