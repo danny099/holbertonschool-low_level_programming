08a2d5a8088bd39911d768b92ed58c813e48d7a0
danny099 <dalmariv01@gmail.com>
2019-10-07 18:49:12 +0000

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+**_strchr - locates a characther in a string
+*
+*@s: is a pointer
+*@c: is a characther
+* Return: On succes dest.
+* On error.
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
