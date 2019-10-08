0251bc938b2be24a56ae158489c76c7469ae7197
danny099 <dalmariv01@gmail.com>
2019-10-08 00:52:09 +0000

    9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*set_string - change bit
+*
+*@s: is a double pointer
+*@to: is a pointer
+* Return: On succes s.
+* On error.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
