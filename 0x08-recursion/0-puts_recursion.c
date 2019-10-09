3c1601fcfe48d24d821bc8588ce35c33ab863e54
danny099 <dalmariv01@gmail.com>
2019-10-09 14:34:33 +0000

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+*_puts_recursion - print a string
+*
+*@s: is a pointer
+* Return: On succes string.
+* On error.
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
