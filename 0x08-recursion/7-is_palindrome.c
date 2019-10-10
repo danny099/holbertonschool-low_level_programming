6de32c26f4ced7b52d2b1b28f9e0c6204f0aa88b
danny099 <dalmariv01@gmail.com>
2019-10-10 22:00:37 +0000

    pali

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+*is_palindrome - is palindrome?
+*
+*@s: is a pointer
+* Return: On succes 1.
+* On error.
+*/
+
+int is_palindrome(char *s)
+{
+	int count, ini, pal;
+
+	ini = 0;
+	count = _strlen_recursion(s);
+	pal = _compare(s, ini, count -1);
+	if (pal == 0)
+		return (0);
+	return (1);
+
+}
+
+int _compare(char *s, int ini, int last)
+{
+
+	
+	if (s[ini] == s[last])
+	{
+		return (_compare(s, ini + 1, last -1));
+	}
+	return (1);
+}
+
+/**
+  *_strlen_recursion - return a size
+  *
+  *@s: is a pointer
+  * Return: On succes number.
+  * On error.
+  */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
