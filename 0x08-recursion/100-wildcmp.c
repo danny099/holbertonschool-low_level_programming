675c0e950151f5332d1a4f64678cbf5e2e7293c1
danny099 <dalmariv01@gmail.com>
2019-10-11 02:47:36 +0000

    wild

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+*wildcmp - print a string
+*
+*@s1: is a pointer
+*@s2: is a pointer
+* Return: On succes int.
+* On error.
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s2 == '*')
+	{
+
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else if (wildcmp(s1, s2 + 1))
+		{
+			return (1);
+		}
+		else
+		{
+			return (wildcmp(s1 + 1, s2));
+		}
+	}
+}
+
+return (0);
+}
