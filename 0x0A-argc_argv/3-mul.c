9878177a7d6fc22285f73f48b8f98cbe7d9c4b49
danny099 <dalmariv01@gmail.com>
2019-10-14 02:41:14 +0000

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - is a main
+*@argc: is a int
+*@argv: is a pointer
+*Return: 0
+	    */
+int main(int argc, char *argv[])
+{
+	int mul;
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	return (0);
+}
