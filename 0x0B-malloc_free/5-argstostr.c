03cde38f88623371d6cc92fb9079abf093a95b9f
danny099 <dalmariv01@gmail.com>
2019-10-16 04:08:50 +0000

    5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+**argstostr - free
+*
+*@av: is a double pointer
+*@ac: is a number
+* Return: On.
+* On error.
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k length;
+	char *m;
+
+	k = 0;
+	length = 0;
+	if (ac <= 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+		}
+		lenth = lenth + j + 1;
+	}
+	m = malloc(sizeof(char) * lenth + 1);
+	if (m == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			m[k] = av[i][j];
+			k++;
+		}
+		m[k] = '\n';
+		k++;
+	}
+	m[k] = '\0';
+	return (m);
+}
