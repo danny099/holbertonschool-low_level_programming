be8f23deac868e0eac887cf27025346b65b5ad58
danny099 <dalmariv01@gmail.com>
2019-10-17 18:49:23 +0000

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* *string_nconcat - concateates two strings
+**s1: is a pointer
+**s2: is a pointer
+* @n: is a unsigned int
+* Return: On succes m.
+* On error null.
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *m;
+	unsigned int i, j, k, l;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (k = 0; s1[k] != '\0'; k++)
+	{
+	}
+	for (l = 0; s2[l] != '\0'; l++)
+	{
+	}
+	if (n >= l)
+		n = l;
+	m = malloc(sizeof(char) * (k + l));
+	if (m == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		*(m + i) = s1[i];
+	}
+	for (j = 0; s2[j] != '\0' && j < n; j++)
+	{
+		*(m + i) = s2[j];
+		i++;
+	}
+	return (m);
+	free(m);
+}
