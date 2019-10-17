e6515070cb75d30abe3da6a2aa383001529f4722
danny099 <dalmariv01@gmail.com>
2019-10-17 19:56:11 +0000

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* *_calloc - calloc with malloc
+* @nmemb: is a unsigned int
+* @size: is a unsigned int
+* Return: On succes m.
+* On error null.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *m;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	m = malloc(nmemb * size);
+	if (m == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= nmemb * size; i++)
+	{
+		*(m + i) = 0;
+	}
+	return (m);
+}
