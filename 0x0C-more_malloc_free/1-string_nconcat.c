90e4e6c3804eb9b7ce8fad257615dace651005a9
danny099 <dalmariv01@gmail.com>
2019-10-17 19:00:17 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		*(m + i) = s1[i];
 	}
-	for (j = 0; s2[j] != '\0' && j < n; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 	{
 		*(m + i) = s2[j];
 		i++;
