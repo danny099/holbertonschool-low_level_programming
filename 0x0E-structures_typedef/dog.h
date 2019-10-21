0a21a07b86b47a56ee9736e3e852444df4eff9b7
danny099 <dalmariv01@gmail.com>
2019-10-21 20:58:46 +0000

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - structure of a dog
+ * @name: First name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
