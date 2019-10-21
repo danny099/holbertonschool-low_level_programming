aa9ddb053af5b962389889614d857d55ac148a6e
danny099 <dalmariv01@gmail.com>
2019-10-21 21:28:45 +0000

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ **_memset - change bit
+ *
+ *@d: is a struct
+ *@name: is a name of the dog
+ *@age: is a age of the dog
+ *@owner: is a owner of the dog
+ * Return: 
+ * On error.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
