5f17dcf8270e40f69485301842e660c8ea5402c8
danny099 <dalmariv01@gmail.com>
2019-10-22 00:13:00 +0000

    .h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 int _strlen(char *s);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
