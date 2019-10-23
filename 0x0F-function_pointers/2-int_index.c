80d625e551e43097df30bff65ff64e8d083d6cd9
danny099 <dalmariv01@gmail.com>
2019-10-23 17:39:29 +0000

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+* int_index - index
+*
+*@array: is a pointer
+*@size: is a size of array
+*@cmp: is a pointer function
+* Return: On succes .
+* On error.
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL || array == NULL)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
