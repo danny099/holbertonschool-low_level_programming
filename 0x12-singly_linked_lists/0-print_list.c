06ed8ea678110ffb00a686f540dd3fb6e0048502
danny099 <dalmariv01@gmail.com>
2019-10-31 17:32:08 +0000

    exercse 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - print a list
+ *
+ *@h: is a pointer
+ * Return: On succes 1.
+ * On error.
+ */
+size_t print_list(const list_t *h)
+{
+	int cont = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)");
+	}
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (cont);
+}
