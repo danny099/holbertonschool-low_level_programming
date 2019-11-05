3a097eba8290ac8568577ce0fae2fbc5b3caea1d
danny099 <dalmariv01@gmail.com>
2019-11-05 14:08:31 +0000

    exercise 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len| - print a list
+ *
+ *@h: is a pointer
+ * Return: On succes 1.
+ * On error.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int cont = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		cont++;
+	}
+	return (cont);
+}
