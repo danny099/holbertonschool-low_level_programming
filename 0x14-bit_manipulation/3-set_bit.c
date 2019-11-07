de2ba1102cf3d9d245380e4b57bac67d8bf81646
danny099 <dalmariv01@gmail.com>
2019-11-07 21:24:40 +0000

    exercise 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * set_bit - get a bit in a position
+ *
+ *@n: is a pointer
+ *@index: is a unsigned int
+ * Return: On succes.
+ * On error.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+	{
+		return  (-1);
+	}
+	*n = *n + (1 << index);
+	return (*n);
+}
