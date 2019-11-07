34774a0af71bcdfa6b05249c625757dc205077d7
danny099 <dalmariv01@gmail.com>
2019-11-07 20:21:37 +0000

    exercise 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * get_bit - get a bit in a position
+ *
+ *@n: is a unsigned long int
+ *@index: is a unsigned int
+ * Return: On succes.
+ * On error.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+	{
+		return(-1);
+	}
+	return (n >> index & 1);
+}
