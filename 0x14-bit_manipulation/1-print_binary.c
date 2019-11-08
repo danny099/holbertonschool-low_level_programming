16d79d93b2bbdf4b6e752c14e0cdc95441124423
danny099 <dalmariv01@gmail.com>
2019-11-08 03:55:24 +0000

    exersice 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@
 
 void print_binary(unsigned long int n)
 {
-	int limit, b;
+	unsigned long int limit, b;
 
 	limit = 1;
 	b = 0;
@@ -24,11 +24,11 @@ void print_binary(unsigned long int n)
 		{
 			if ((n & limit) == 0 && b)
 			{
-				printf("0");
+				putchar('0');
 			}
 			if ((n & limit) != 0)
 			{
-				printf("1");
+				putchar('1');
 				b = 1;
 			}
 			limit >>= 1;
