0cd5145cd9d24b6fa11a4dc86b970df47aca4121
danny099 <dalmariv01@gmail.com>
2019-11-12 21:51:20 +0000

    exercise 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	file = open(filename, O_APPEND | O_WRONLY, 0600);
+	file = open(filename, O_APPEND | O_WRONLY);
 	if (text_content == NULL)
 	{
 		return (1);
