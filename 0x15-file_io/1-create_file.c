4bd7c85c7f5c560d1e4d2c4e6cc6d7c0920b43c6
danny099 <dalmariv01@gmail.com>
2019-11-12 20:48:14 +0000

    exercise 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * create_file - create a file
+ *
+ *@filename: is a pointer
+ *@text_content: is a pointer
+ * Return: On succes.
+ * On error.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, i, wr;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	for (i = 0; text_content[i]; i++)
+	{
+	}
+	wr = write(file, text_content, i);
+	if (wr == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
+	return (1);
+}
