6432de25f563c7c683a5185af0bb4ed779c2dc73
danny099 <dalmariv01@gmail.com>
2019-11-12 21:01:13 +0000

    exercise 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	for (i = 0; text_content[i]; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 	{
 	}
 	wr = write(file, text_content, i);
