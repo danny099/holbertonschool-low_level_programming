94e39a916cc7372733b19a65cd85db48811b6d34
danny099 <dalmariv01@gmail.com>
2019-12-23 05:15:13 +0000

    exercise 3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - print a list
+ *
+ *@head: is a double pointer
+ *@n: is a const int
+ * Return: On succes 1.
+ * On error.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp = *head, *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+	{
+		return (0);
+	}
+	node->n = n;
+	node->next = NULL;
+	if (tmp != NULL)
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		new->prev = tmp;
+		tmp->next = node;
+	}
+	else
+	{
+		node->prev = NULL;
+		*head = node;
+	}
+	return
+		(*head);
+}
