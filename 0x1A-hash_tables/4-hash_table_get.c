944e9de7c9cbbc55a871dd619431cd6275fda238
danny099 <dalmariv01@gmail.com>
2020-01-20 06:06:11 +0000

    exercise 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get -  ger hast
+ * @ht: is a hash_table_t
+ * @key: is achar
+ * Return:  0
+ **/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL)
+    {
+		return (NULL);
+    }
+	index = key_index((unsigned char *)key, ht->size);
+	new_node = ht->array[index];
+	while (new_node)
+	{
+		if (strcmp(new_node->key, key) == 0)
+        {
+			return (new_node->value);
+        }
+        else
+		    new_node = new_node->next;
+	}
+	return (NULL);
+}
