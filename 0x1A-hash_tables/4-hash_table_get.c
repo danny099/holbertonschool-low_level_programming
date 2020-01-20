5e1bdb4663b0a5cfb9b2b7d12b9cedbe6d6bbe06
danny099 <dalmariv01@gmail.com>
2020-01-20 06:08:16 +0000

    exercise 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,18 +11,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *new_node;
 
 	if (ht == NULL || key == NULL)
-    {
+	{
 		return (NULL);
-    }
+	}
 	index = key_index((unsigned char *)key, ht->size);
 	new_node = ht->array[index];
 	while (new_node)
 	{
 		if (strcmp(new_node->key, key) == 0)
-        {
+		{
 			return (new_node->value);
-        }
-        else
+		}
+		else
 		    new_node = new_node->next;
 	}
 	return (NULL);
